e05550a9d0e07c1447a47539482641e8e50e24d8
Deekshith1994 <kdeekshith1994@gmail.com>
2017-04-06 15:09:34 -0400

    "4/6/2017"

--- test.py
+++ test.py
@@ -30,14 +30,14 @@ for i in range(0,len(lines)):
     if(lines[i].strip()!= ""):
         print(lines[i].strip());
         try:
-            # time.sleep(4)
-            # req = Request('http://dl.acm.org/citation.cfm?id=' + lines[i].strip(), headers=headers)#1965725
-            # htmlfile = urlopen(req)
-            # soup = BeautifulSoup(htmlfile, "html.parser")
-            # temp = soup.find('a', {"name": "FullTextPDF"})['href']
-            # address = 'http://dl.acm.org/' + temp
-            # print(address)
-            address = "http://delivery.acm.org/10.1145/1970000/1965725/p5-vardi.pdf?ip=152.15.236.134&id=1965725&acc=OPEN&key=A79D83B43E50B5B8%2E48786266F2419CCD%2E4D4702B0C3E38B35%2E7BDEF374746A56FB&CFID=745020279&CFTOKEN=36770104&__acm__=1490888820_cda4f94c6173722e0411de005b5bf9a7"
+            time.sleep(4)
+            req = Request('http://dl.acm.org/citation.cfm?id=' + lines[i].strip(), headers=headers)#1965725
+            htmlfile = urlopen(req)
+            soup = BeautifulSoup(htmlfile, "html.parser")
+            temp = soup.find('a', {"name": "FullTextPDF"})['href']
+            address = 'http://dl.acm.org/' + temp
+            print(address)
+
             time.sleep(4)
             req = Request(address, headers={'User-Agent': 'chrome'})
             response = urlopen(req)
@@ -52,8 +52,8 @@ for i in range(0,len(lines)):
                 page_content += pg.extractText()
 
             page_content = page_content.replace("\n","")
-            # firebaseR = firebase.FirebaseApplication('https://pique-fa32e.firebaseio.com/')
-            # result = firebaseR.put('/ResearchPapers_fullText', lines[i].strip(), page_content)
+            firebaseR = firebase.FirebaseApplication('https://pique-fa32e.firebaseio.com/')
+            result = firebaseR.put('/ResearchPapers_FullText', lines[i].strip(), page_content)
             print(page_content)
         except:
             # address = "http://delivery.acm.org/10.1145/1970000/1965725/p5-vardi.pdf?ip=152.15.236.134&id=1965725&acc=OPEN&key=A79D83B43E50B5B8%2E48786266F2419CCD%2E4D4702B0C3E38B35%2E7BDEF374746A56FB&CFID=745020279&CFTOKEN=36770104&__acm__=1490888820_cda4f94c6173722e0411de005b5bf9a7"
